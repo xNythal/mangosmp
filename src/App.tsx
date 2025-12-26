f097b1650a7b9914a6a4b9a99274e6fb9b1df1ba
xNythal <xNythal@users.noreply.github.com>
2025-12-27 00:01:18 +0200

    Made routes dynamic and fixed 404 bug with gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { About, HomeLayout, Landing, Error } from "./pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { About, HomeLayout, Landing, Error } from "./pages"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeLayout />,
-    errorElement: <Error />,
-    children: [
-      { index: true, element: <Landing /> },
-      { path: "about", element: <About /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <HomeLayout />,
+      errorElement: <Error />,
+      children: [
+        { index: true, element: <Landing /> },
+        { path: "about", element: <About /> },
+      ],
+    },
+  ],
+  { basename: import.meta.env.VITE_BASENAME },
+)
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 }
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/supercraft/",
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
-})
+export default ({ mode }: { mode: "production" | "development" }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_")
+  return defineConfig({
+    base: env.VITE_BASENAME,
+    plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+    ],
+  })
+}
